6325b7e24b4656c72f6ff4cc261c4b9f9e89cdec
JonSelby Co <jonathan.selby@nike.com>
2021-04-22 00:01:48 -0700

    Updated Styling For Mobile Header

--- pages/index.vue
+++ pages/index.vue
@@ -44,19 +44,18 @@ export default Vue.extend({})
 </script>
 
 <style lang="scss">
-a{
+a {
   color: #131514;
   font-size: 18px;
   font-weight: 600;
   text-decoration: none;
 
-  &:hover{
+  &:hover {
 color: #E3B047;
   }
 }
 
- .header-container{
-   border: 2px dashed hotpink;
+ .header-container {
    background-color: rgba(255, 255, 255, .15);
     backdrop-filter: blur(5px);
    display: flex;
@@ -66,16 +65,37 @@ color: #E3B047;
    position: sticky;
    top: 0;
 
-   div{
-padding: 30px 0;
+   div {
+    padding: 30px 0;
+   }
+
+   @media only screen and (max-width: 480px)  {
+     justify-content: center;
+     align-content: center;
+     flex-direction: column;
+
+     div {
+       display: flex;
+       justify-content: center;
+     }
+
+     .header-links {
+       margin-left: -20px;
+
+       li{
+         &:last-child{
+           padding-right: 30px;
+         }
+       }
+     }
    }
  }
 
- .header-links{
+ .header-links {
    display: flex;
    list-style: none;
 
-   li{
+   li {
      padding: 0 20px;
    }
  }

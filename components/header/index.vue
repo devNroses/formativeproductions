bef94bb392e41a93d94600994b17aa6a1899210a
JonSelby Co <jonathan.selby@nike.com>
2021-04-22 00:21:54 -0700

    Created Header Component and Imported into Page

---
+++ components/header/index.vue
@@ -0,0 +1,71 @@
+<template>
+      <div class="header-container">
+      <div>Logo</div>
+      <div>
+        <ul class="header-links">
+          <li><nuxt-link to="/work">Work</nuxt-link></li>
+          <li><nuxt-link to="/about">About</nuxt-link></li>
+          <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<style lang="scss" scope>
+a {
+  color: #131514;
+  font-size: 18px;
+  font-weight: 600;
+  text-decoration: none;
+
+  &:hover {
+color: #E3B047;
+  }
+}
+
+ .header-container {
+   background-color: rgba(255, 255, 255, .15);
+    backdrop-filter: blur(5px);
+   display: flex;
+   justify-content: space-between;
+   padding: 0 50PX;
+   min-height: 60px;
+   position: sticky;
+   top: 0;
+
+   div {
+    padding: 30px 0;
+   }
+
+   @media only screen and (max-width: 480px)  {
+     justify-content: center;
+     align-content: center;
+     flex-direction: column;
+
+     div {
+       display: flex;
+       justify-content: center;
+     }
+
+     .header-links {
+       margin-left: -20px;
+
+       li{
+         &:last-child{
+           padding-right: 30px;
+         }
+       }
+     }
+   }
+ }
+
+ .header-links {
+   display: flex;
+   list-style: none;
+
+   li {
+     padding: 0 20px;
+   }
+ }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="container">
-    <div class="header-container">
-      <div>Logo</div>
-      <div>
-        <ul class="header-links">
-          <li><nuxt-link to="/work">Work</nuxt-link></li>
-          <li><nuxt-link to="/about">About</nuxt-link></li>
-          <li><nuxt-link to="/contact">Contact</nuxt-link></li>
-        </ul>
-      </div>
-    </div>
+  <Header />
     <!-- <div>
       <Logo />
       <h1 class="title">
@@ -39,65 +30,10 @@
 
 <script lang="ts">
 import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
-
-<style lang="scss">
-a {
-  color: #131514;
-  font-size: 18px;
-  font-weight: 600;
-  text-decoration: none;
-
-  &:hover {
-color: #E3B047;
+import Header from '../components/header/index.vue'
+export default Vue.extend({
+  components: {
+    Header
   }
-}
-
- .header-container {
-   background-color: rgba(255, 255, 255, .15);
-    backdrop-filter: blur(5px);
-   display: flex;
-   justify-content: space-between;
-   padding: 0 50PX;
-   min-height: 60px;
-   position: sticky;
-   top: 0;
-
-   div {
-    padding: 30px 0;
-   }
-
-   @media only screen and (max-width: 480px)  {
-     justify-content: center;
-     align-content: center;
-     flex-direction: column;
-
-     div {
-       display: flex;
-       justify-content: center;
-     }
-
-     .header-links {
-       margin-left: -20px;
-
-       li{
-         &:last-child{
-           padding-right: 30px;
-         }
-       }
-     }
-   }
- }
-
- .header-links {
-   display: flex;
-   list-style: none;
-
-   li {
-     padding: 0 20px;
-   }
- }
-
-</style>
+})
+</script>

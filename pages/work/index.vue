73036d2666e6a22fc6ad9df46ce90bd39617df0b
JonSelby Co <jonathan.selby@nike.com>
2021-05-14 11:58:24 -0700

    Added Video Iframe to Modal

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -9,17 +9,20 @@
     </div>
 
     <div class="section">
-      <div class="section-video work-video">
+      <div
+        @click="toggleShowVideo('536417585')"
+        class="section-video work-video"
+      >
         <img src="~/assets/img/demo-reel.png" />
       </div>
     </div>
 
     <div class="section">
-      <div class="work-card">
-        <div @click="toggleShowVideo" class="section-video work-video">
+      <div class="work-card" @click="toggleShowVideo('487463591')">
+        <div class="section-video work-video">
           <img src="~/assets/img/colorado-springs-museum.png" />
         </div>
-        <p class="work-card-title">Colorado Springs museum</p>
+        <p class="work-card-title">Real Peopl / Real Story</p>
         <p class="work-card-content">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -27,7 +30,7 @@
           aliquip ex ea commodo consequat.
         </p>
       </div>
-      <div class="work-card">
+      <div class="work-card" @click="toggleShowVideo('536427164')">
         <div class="section-video work-video">
           <img src="~/assets/img/football-season.png" />
         </div>
@@ -41,31 +44,37 @@
       </div>
     </div>
 
-    <transition name="fade" appear>
-      <div v-if="show" @click="toggleShowVideo" class="modal-overlay" />
-    </transition>
-    <transition name="pop" appear>
-      <div class="modal" role="dialog" v-if="show">
-        <p>Show Video</p>
-        <button @click="toggleShowVideo" class="button">
-          Hide Modal
-        </button>
-      </div>
-    </transition>
+    <VideoModal :showModal="show" :viewVideoid="workVideoId" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-@Component
-export default class Work extends Vue {
-  show: boolean = false;
-
-  toggleShowVideo() {
-    this.show = !this.show;
+import { Vue } from "vue-property-decorator";
+import VideoModal from "../../components/videoModal/index.vue";
+export default Vue.extend({
+  components: {
+    VideoModal
+  },
+  data() {
+    return {
+      show: false,
+      workVideoId: ""
+    };
+  },
+  methods: {
+    toggleShowVideo(videoId: string) {
+      this.workVideoId = videoId;
+      this.show = !this.show;
+    }
   }
-}
+});
 </script>
+
+<style lang="scss">
+iframe {
+  width: 100% !important;
+}
+</style>
 
 <style lang="scss" scoped>
 .work {
@@ -99,7 +108,7 @@ export default class Work extends Vue {
     align-items: center;
     overflow: hidden;
     height: 300px;
-    max-width: 750px;
+    max-width: 950px;
     margin: 0 auto;
 
     img {
@@ -118,6 +127,10 @@ export default class Work extends Vue {
     max-width: 500px;
     height: 100%;
     width: 100%;
+
+    &:hover {
+      cursor: pointer;
+    }
 
     &-title {
       padding-top: 20px;
@@ -144,63 +157,3 @@ export default class Work extends Vue {
     }
   }
 }
-
-.modal {
-  position: absolute;
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  margin: auto;
-  text-align: center;
-  width: fit-content;
-  height: fit-content;
-  max-width: 22em;
-  padding: 2rem;
-  border-radius: 1rem;
-  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2);
-  background: #fff;
-  z-index: 999;
-  transform: none;
-}
-.modal h1 {
-  margin: 0 0 1rem;
-}
-
-.modal-overlay {
-  content: "";
-  position: absolute;
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 998;
-  background: #2c3e50;
-  opacity: 0.6;
-  cursor: pointer;
-}
-
-/* ---------------------------------- */
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.4s linear;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-
-.pop-enter-active,
-.pop-leave-active {
-  transition: transform 0.4s cubic-bezier(0.5, 0, 0.5, 1), opacity 0.4s linear;
-}
-
-.pop-enter,
-.pop-leave-to {
-  opacity: 0;
-  transform: scale(0.3) translateY(-50%);
-}
-</style>

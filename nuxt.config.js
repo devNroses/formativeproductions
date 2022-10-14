63703ccd55ef23843fc0d8304784b85c53057db6
JonSelby Co <jons@selbygroupco.com>
2022-10-13 21:45:59 -0700

    (config) updating site version
    also applied a linter as well to the project

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,32 +3,28 @@ export default {
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   head: {
-    title: 'Formative Productions',
+    title: "Formative Productions",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '~/assets/img/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "~/assets/img/favicon.ico" }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/style/main.scss'
-  ],
+  css: ["~/assets/style/main.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/vue-video-wrapper.js'
-  ],
+  plugins: ["~/plugins/vue-video-wrapper.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -36,20 +32,19 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
+    "@nuxt/typescript-build"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/svg'
+    "@nuxtjs/axios",
+    "@nuxtjs/svg"
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};

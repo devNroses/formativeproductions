30779986aef440d848be84e6912091fb0782977d
JonSelby Co <jons@selbygroupco.com>
2022-10-30 00:47:45 -0700

    initial build of feature video

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import React from 'react';
 
 import Header from './components/Header/Header';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <video width="320" height="240" controls autoPlay={true}>
+        <source src="https://vimeo.com/536417585?embedded=true&source=vimeo_logo&owner=122570197" />
+      </video>
     </div>
   );
-}
+};
 
 export default App;

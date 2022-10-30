6f0315afebdbbf66cb835143e8fb69f5fb858114
JonSelby Co <jons@selbygroupco.com>
2022-10-30 00:21:59 -0700

    updating header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,28 @@
+import './header-styles.scss';
+
 import React from 'react';
 
-const Header = () => {
-  <div>Header</div>;
+import Logo2 from '../../assets/images/formative-logo.png';
+
+const Header: React.FC = () => {
+  return (
+    <nav className="nav-container">
+      <div className="nav-logo">
+        <img
+          style={{ width: "100%", height: "40px" }}
+          src={Logo2}
+          alt="Formative Logo"
+        />
+      </div>
+      <div className="nav-pages">
+        <ul>
+          <li>Work</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
